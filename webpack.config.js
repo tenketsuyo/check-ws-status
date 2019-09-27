f918719d9f22a065223e28b9f2db836963b81fbd
tenketsu <tenketsu@kopr.ru>
2019-09-27 17:01:46 +0300

    +readme.md

--- webpack.config.js
+++ webpack.config.js
@@ -47,17 +47,12 @@ module.exports = {
     ],
     devServer: {
         historyApiFallback: true,
-        // proxy: {
-        //     '/ServiceCheckerWs': 'http://tomcat2.kopr:8185'
-        // },
-        host: '172.18.6.64',
-        port: 8080
     },
     externals: {
         // global app config object
         config: JSON.stringify({
             apiUrl: 'http://localhost:4000',
-            urls: 'http://tomcat2.kopr:8185/ServiceCheckerWs/rest/check/isAvaliable|http://172.18.5.76:8185/ServiceCheckerWs/rest/check/isAvaliable'
+            urls: 'url1|url2'
         })
     }
 }
